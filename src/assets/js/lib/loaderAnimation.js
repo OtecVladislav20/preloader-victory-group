a69773672a9972593ba6a4f0fdd25808e7b56c5e
Vladislav Maslakov <maslakov20@mail.ru>
2025-09-17 17:33:19 +0500

    Add animation to hide screen

--- src/assets/js/lib/loaderAnimation.js
+++ src/assets/js/lib/loaderAnimation.js
@@ -1,36 +1,52 @@
 export default function loaderAnimation() {
-    const progressBar = document.querySelector('.loadbar');
-        const percentElement = document.querySelector('.preloader__percent');
+    const progressBar = document.querySelector('.preloader__footer__loading__bar__wrapper__loadbar');
+    const percentElement = document.querySelector('.preloader__footer__loading__percent');
+    const percentScreen = document.querySelector('.preloader');
 
-        if (!progressBar || !percentElement) {
-            console.error('Элементы для анимации не найдены!');
-            return;
-        }
+    if (!progressBar || !percentElement || !percentScreen) {
+        console.error('Элементы для анимации не найдены!');
+        return;
+    }
+    
+    let progress = 0;
+    const targetProgress = 100;
+    const duration = 5000;
+    const interval = 50;
+    const steps = duration / interval;
+    const increment = targetProgress / steps;
+    
+    const animateProgress = () => {
+        const timer = setInterval(() => {
+            progress += increment;
+            
+            if (progress >= targetProgress) {
+                progress = 100;
+                clearInterval(timer);
+                setTimeout(() => {
+                    console.log('Загрузка завершена!');
+                    hidePreloaderScreen();
+                }, 500);
+            }
+            
+            progressBar.style.width = progress + '%';
+            percentElement.textContent = Math.round(progress) + '%';
+            
+        }, interval);
+    };
+
+    function hidePreloaderScreen() {
+        percentScreen.style.transition = 'top 0.8s ease-in-out';
+        percentScreen.style.top = '-100vh';
         
-        let progress = 0;
-        const targetProgress = 100;
-        const duration = 5000;
-        const interval = 50;
-        const steps = duration / interval;
-        const increment = targetProgress / steps;
-        
-        const animateProgress = () => {
-            const timer = setInterval(() => {
-                progress += increment;
-                
-                if (progress >= targetProgress) {
-                    progress = 100;
-                    clearInterval(timer);
-                    setTimeout(() => {
-                        console.log('Загрузка завершена!');
-                    }, 500);
-                }
-                
-                progressBar.style.width = progress + '%';
-                percentElement.textContent = Math.round(progress) + '%';
-                
-            }, interval);
-        };
-        
-        setTimeout(animateProgress, 300);
+        setTimeout(() => {
+            percentScreen.style.display = 'none';
+            
+            document.body.style.overflow = 'auto';
+        }, 1000);
+    }
+    
+    document.body.style.overflow = 'hidden';
+    
+    setTimeout(animateProgress, 300);
 }
+
